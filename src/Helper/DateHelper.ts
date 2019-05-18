6a3c6fe78996807312d14e5391910611c3bbabc0
11111991 <i1111991@yahoo.com>
2019-05-18 21:52:36 +0700

    * remove dead code

--- src/Helper/DateHelper.ts
+++
@@ -1,8 +0,0 @@
-export const DateHelper = {
-    toFormat(date: Date, format: string): string {
-        return format.replace(/dd/ig, this.toFill(date.getDate()));
-    },
-    toFill(num: number): string {
-        return (num > 10) ? num.toString() : ("0" + num).slice(-2);
-    }
-};
