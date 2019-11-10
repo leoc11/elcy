d4a4c74a907ece0501861988d4ed1fd125a66bbd
leo.c <leo@intersoftpt.com>
2019-11-10 21:50:24 +0700

    * fix Defer

--- src/Common/Defer.ts
+++ src/Common/Defer.ts
@@ -1,11 +1,15 @@
 export class Defer<T> extends Promise<T> {
     constructor() {
+        let _resolve: (value?: T | PromiseLike<T>) => void;
+        let _reject: (reason?: any) => void;
         super((resolve, reject) => {
-            this.resolve = resolve;
-            this.reject = reject;
+            _resolve = resolve;
+            _reject = reject;
         });
+        this.resolve = _resolve;
+        this.reject = _reject;
     }
-
     public resolve: (value?: T | PromiseLike<T>) => void;
     public reject: (reason?: any) => void;
 }
+Defer.prototype.constructor = Promise;
