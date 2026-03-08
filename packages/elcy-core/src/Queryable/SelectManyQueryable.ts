ce9c3934cb15e5eace7ce5e3a08a8df247076828
Leo Chandra <leo.chandra11@gmail.com>
2026-03-09 00:24:06 +0700

    * enumerable, queryable: typing groupJoin, pivot, flatMap

--- packages/elcy-core/src/Queryable/SelectManyQueryable.ts
+++ packages/elcy-core/src/Queryable/SelectManyQueryable.ts
@@ -1,3 +1,4 @@
+import { IEnumerable } from "@elcy/enumerable";
 import { GenericType } from "../Common/Type";
 import { FunctionExpression } from "../ExpressionBuilder/Expression/FunctionExpression";
 import { MethodCallExpression } from "../ExpressionBuilder/Expression/MethodCallExpression";
@@ -12,14 +13,14 @@ import { SelectExpression } from "./QueryExpression/SelectExpression";
 export class SelectManyQueryable<S, T> extends Queryable<T> {
     protected get selector() {
         if (!this._selector && this.selectorFn) {
-            this._selector = ExpressionBuilder.parse<Iterable<T>>(this.selectorFn, [this.parent.type], this.parameters);
+            this._selector = ExpressionBuilder.parse<IEnumerable<T>>(this.selectorFn, [this.parent.type], this.parameters);
         }
         return this._selector;
     }
     protected set selector(value) {
         this._selector = value;
     }
-    constructor(public readonly parent: Queryable<S>, selector: FunctionExpression<Iterable<T>> | ((item: S) => Iterable<T>), public type: GenericType<T> = Object) {
+    constructor(public readonly parent: Queryable<S>, selector: FunctionExpression<IEnumerable<T>> | ((item: S) => IEnumerable<T>), public type: GenericType<T> = Object) {
         super(type, parent);
         if (selector instanceof FunctionExpression) {
             this.selector = selector;
@@ -28,10 +29,10 @@ export class SelectManyQueryable<S, T> extends Queryable<T> {
             this.selectorFn = selector;
         }
     }
-    protected _selector: FunctionExpression<Iterable<T>>;
-    protected readonly selectorFn: ((item: S) => Iterable<T>);
+    protected _selector: FunctionExpression<IEnumerable<T>>;
+    protected readonly selectorFn: ((item: S) => IEnumerable<T>);
     public buildQuery(queryVisitor: IQueryVisitor): IQueryExpression<T> {
-        const objectOperand = this.parent.buildQuery(queryVisitor) as SelectExpression<S>;
+        const objectOperand = this.parent.buildQuery(queryVisitor) as SelectExpression<S & object>;
         const methodExpression = new MethodCallExpression(objectOperand, "flatMap", [this.selector.clone()]);
         const visitParam: IQueryVisitParameter = { selectExpression: objectOperand, scope: "queryable" };
         const result = queryVisitor.visit(methodExpression, visitParam) as SelectExpression;
--- packages/enumerable/src/Interface/Type.ts
+++ packages/enumerable/src/Interface/Type.ts
@@ -1,3 +1,5 @@
+import type { Enumerable } from "src/Enumerable";
+
 export type OrderDirection = "ASC" | "DESC";
 export type IObjectType<T = unknown> = { new (...values: unknown[]): T };
 export type GenericType<T = unknown> =
@@ -6,7 +8,7 @@ export type GenericType<T = unknown> =
 export type Pivot<
   T,
   TD extends { [key: string]: (item: T) => unknown },
-  TM extends { [key: string]: (item: T[]) => unknown },
+  TM extends { [key: string]: (item: Enumerable<T>) => unknown },
 > = { [key in keyof TD]: ReturnType<TD[key]> } & {
   [key in keyof TM]: ReturnType<TM[key]>;
 };
