ab622b6d38c4d6d677736220e213d036ea976f1a
Leo Chandra <leo.chandra11@gmail.com>
2026-03-28 11:14:58 +0700

    * (core) expression: typing

--- packages/elcy-core/src/ExpressionBuilder/Expression/TypeofExpression.ts
+++ packages/elcy-core/src/ExpressionBuilder/Expression/TypeofExpression.ts
@@ -1,9 +1,10 @@
+import { PrimitiveType } from "src/Common/Type";
 import { hashCode, hashCodeAdd, resolveClone } from "../../Helper/Util";
 import { IExpression } from "./IExpression";
 import { IUnaryOperatorExpression } from "./IUnaryOperatorExpression";
 export class TypeofExpression implements IUnaryOperatorExpression<string> {
     constructor(public operand: IExpression) { }
-    public type = String;
+    public type: PrimitiveType<string> = String;
     public clone(replaceMap?: Map<IExpression, IExpression>) {
         if (!replaceMap) {
             replaceMap = new Map();
