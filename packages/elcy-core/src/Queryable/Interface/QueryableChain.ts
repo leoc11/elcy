5a6f24ffd6743e0f1bd7956b7b9e517f12f5796a
Leo Chandra <leo.chandra11@gmail.com>
2026-04-12 23:47:33 +0700

    * typing: QuryableChain for IGroupArray

--- packages/elcy-core/src/Queryable/Interface/QueryableChain.ts
+++ packages/elcy-core/src/Queryable/Interface/QueryableChain.ts
@@ -5,15 +5,16 @@ import type Decimal from "decimal.js";
 import { IGroupArray } from "src/Common/IGroupArray";
 import { ValueType } from "src/Common/Type";
 
-export type QueryableChain<T> = T extends Queryable<infer U> ? QueryableArrayChain<U> & QueryaChainMarker<T>
-    : T extends GroupedEnumerable<infer K, infer U> ? QueryableGroupArrayChain<K, U> & QueryaChainMarker<T>
-    : T extends Enumerable<infer U> ? QueryableArrayChain<U> & QueryaChainMarker<T>
-    : T extends Array<infer U> ? QueryableArrayChain<U> & QueryaChainMarker<T>
+export type QueryableChain<T> = T extends Queryable<infer U> ? QueryableArrayChain<U> & QueryChainMarker<T>
+    : T extends GroupedEnumerable<infer K, infer U> ? QueryableGroupArrayChain<K, U> & QueryChainMarker<T>
+    : T extends Enumerable<infer U> ? QueryableArrayChain<U> & QueryChainMarker<T>
+    : T extends IGroupArray<infer U, infer K> ? QueryableGroupArrayChain<K, U> & QueryChainMarker<T>
+    : T extends Array<infer U> ? QueryableArrayChain<U> & QueryChainMarker<T>
     : T extends MainValueType ? T
     : T extends object ? QueryableObjectChain<T>
     : T;
 
-export type Unchain<T> = T extends QueryaChainMarker<infer U> ? U
+export type Unchain<T> = T extends QueryChainMarker<infer U> ? U
     : T extends GroupedEnumerable<infer K, infer U> ? IGroupArray<K, U>
     : T extends Enumerable<infer U> ? Unchain<U>[]
     : T extends Array<infer U> ? Unchain<U>[]
@@ -21,18 +22,18 @@ export type Unchain<T> = T extends QueryaChainMarker<infer U> ? U
     : T;
 
 declare const type: unique symbol;
-type QueryaChainMarker<T> = {
+type QueryChainMarker<T> = {
     [type]: T
 };
 type MainValueType = number | bigint | string | boolean | Date | ArrayBufferView | ArrayBuffer | Temporal.Instant | Temporal.PlainDate | Temporal.PlainTime | Decimal;
 type QueryableObjectChain<T extends object> = {
     [P in keyof T]-?
-    : NonNullable<T[P]> extends GroupedEnumerable<infer K, infer U> ? QueryableGroupArrayChain<K, U> & QueryaChainMarker<T[P]>
-    : NonNullable<T[P]> extends Enumerable<infer U> ? QueryableArrayChain<U> & QueryaChainMarker<T[P]>
-    : NonNullable<T[P]> extends Array<infer U> ? QueryableArrayChain<U> & QueryaChainMarker<T[P]>
+    : NonNullable<T[P]> extends GroupedEnumerable<infer K, infer U> ? QueryableGroupArrayChain<K, U> & QueryChainMarker<T[P]>
+    : NonNullable<T[P]> extends Enumerable<infer U> ? QueryableArrayChain<U> & QueryChainMarker<T[P]>
+    : NonNullable<T[P]> extends Array<infer U> ? QueryableArrayChain<U> & QueryChainMarker<T[P]>
     : NonNullable<T[P]> extends MainValueType ? T[P]
     : NonNullable<T[P]> extends object ? QueryableObjectChain<NonNullable<T[P]>>
     : T[P];
-} & QueryaChainMarker<T>;
+} & QueryChainMarker<T>;
 type QueryableArrayChain<T> = Enumerable<QueryableChain<T>> & { loads: (...includes: Array<(item: QueryableChain<T>) => Exclude<object, ValueType>>) => QueryableArrayChain<T> };
 type QueryableGroupArrayChain<K, T> = Enumerable<QueryableChain<T>> & { key: QueryableChain<K>, loads: (...includes: Array<(item: QueryableChain<T>) => Exclude<object, ValueType>>) => QueryableArrayChain<T> };
