b2e6b422ff811f33cbb89e349fed06d5bdaf95dd
Leo Chandra <leo.chandra11@gmail.com>
2026-06-06 16:15:20 +0700

    * Decorator: CheckContraint es decorator

--- packages/elcy-core/src/Decorator/CheckConstraint.ts
+++ packages/elcy-core/src/Decorator/CheckConstraint.ts
@@ -1,47 +1,48 @@
 import { ArrayExtension } from "src/Extensions/ArrayExtension";
-import { IObjectType, StringKeyOf } from "../Common/Type";
-import { AbstractEntityMetaData } from "../MetaData/AbstractEntityMetaData";
+import { IObjectType } from "../Common/Type";
 import { CheckConstraintMetaData } from "../MetaData/CheckConstraintMetaData";
-import { getEntityMetadata, setEntityMetadata } from "../MetaData/MetaDataMapper";
 import { ICheckConstraintOption } from "./Option/ICheckConstraintOption";
+import { ClassDecorator } from "./Type";
+import { IEntityMetaData } from "src/MetaData";
 
-export function CheckContraint<TE extends object = object>(option: ICheckConstraintOption<TE>): ClassDecorator & PropertyDecorator & MethodDecorator;
-export function CheckContraint<TE extends object = object>(check: (entity: TE) => boolean): ClassDecorator & PropertyDecorator & MethodDecorator;
-export function CheckContraint<TE extends object = object>(name: string, check: (entity: TE) => boolean): ClassDecorator & PropertyDecorator & MethodDecorator;
-export function CheckContraint<TE extends object = object>(optionOrCheckOrName: ICheckConstraintOption | string | ((entity: TE) => boolean), check?: (entity: TE) => boolean): ClassDecorator & PropertyDecorator & MethodDecorator {
+export function CheckContraint<TE extends object = object>(option: ICheckConstraintOption<TE>): ClassDecorator<IObjectType<TE>>;
+export function CheckContraint<TE extends object = object>(check: (entity: TE) => boolean): ClassDecorator<IObjectType<TE>>;
+export function CheckContraint<TE extends object = object>(name: string, check: (entity: TE) => boolean): ClassDecorator<IObjectType<TE>>;
+export function CheckContraint<TE extends object = object>(optionOrCheckOrName: ICheckConstraintOption | string | ((entity: TE) => boolean), check?: (entity: TE) => boolean): ClassDecorator<IObjectType<TE>> {
     let option: ICheckConstraintOption<TE> = {} as any;
     switch (typeof optionOrCheckOrName) {
         case "object":
-            option = optionOrCheckOrName as any;
+            option = optionOrCheckOrName as ICheckConstraintOption<TE>;
             break;
         case "function":
-            option.check = optionOrCheckOrName as any;
+            check = optionOrCheckOrName;
             break;
         case "string":
-            option.name = optionOrCheckOrName as any;
+            option.name = optionOrCheckOrName;
             break;
     }
+
     if (check) {
         option.check = check;
     }
 
-    return <T, TC extends Function = IObjectType<TE>>(target: TC | TE, propertyKey?: StringKeyOf<TE>, descriptor?: TypedPropertyDescriptor<T>) => {
-        const entConstructor: IObjectType<TE> = propertyKey ? target.constructor as IObjectType<TE> : target as IObjectType<TE>;
+    return (type: IObjectType<TE>, context: ClassDecoratorContext<IObjectType<TE>>) => {
+        let handlers = context.metadata.behaviors as Array<(entityMeta: IEntityMetaData<TE>) => void>;
+        if (!Array.isArray(handlers)) {
+            context.metadata.behaviors = handlers = [];
+        }
+
         if (!option.name) {
-            option.name = `CK_${entConstructor.name}_${(propertyKey ? propertyKey : "")}`;
+            option.name = `CK_${type.name}`;
         }
 
-        let entityMetaData = getEntityMetadata(entConstructor);
-        if (entityMetaData == null) {
-            entityMetaData = new AbstractEntityMetaData(entConstructor);
-        }
-
-        let checkMetaData = entityMetaData.constraints.find((o) => o instanceof CheckConstraintMetaData && o.name === option.name);
-        if (checkMetaData) {
-            ArrayExtension.delete(entityMetaData.constraints, checkMetaData);
-        }
-        checkMetaData = new CheckConstraintMetaData(option.name, entityMetaData, option.check);
-        entityMetaData.constraints.push(checkMetaData);
-        setEntityMetadata(entConstructor, entityMetaData);
+        handlers.push((entityMeta) => {
+            let checkMetaData = entityMeta.constraints.find((o) => o instanceof CheckConstraintMetaData && o.name === option.name);
+            if (checkMetaData) {
+                ArrayExtension.delete(entityMeta.constraints, checkMetaData);
+            }
+            checkMetaData = new CheckConstraintMetaData(option.name, entityMeta, option.check);
+            entityMeta.constraints.push(checkMetaData);
+        });
     };
 }
